db82338d6a48646d7cf93b6ce1f9240864c5bb00
himanshu <anshmishra75436@gmail.com>
2025-12-23 15:16:55 +0530

    [Himanhsu Mishra] Add . fabonacci series code review

---
+++ reviews/FabonacciSeries.java
@@ -0,0 +1,19 @@
+package reviews;
+
+public class FabonacciSeries {
+    public static void main(String[] args){
+
+		Scanner sc=new Scanner(System.in){
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int count=0;
+		for(int i=0;i<9;i++){
+		count++;
+		}
+		int a=a+b;
+		int b=b+a;
+		}
+
+
+    
+}
