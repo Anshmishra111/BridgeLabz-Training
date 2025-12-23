dba7b13f29dfa112ede2ba7f5e516148d5550836
himanshu <anshmishra75436@gmail.com>
2025-12-23 15:59:35 +0530

    [Himanshu Mishra] Add. code review fabonaaci series

--- reviews/FabonacciSeries.java
+++ reviews/FabonacciSeries.java
@@ -3,15 +3,18 @@ package reviews;
 public class FabonacciSeries {
     public static void main(String[] args){
 
-		Scanner sc=new Scanner(System.in){
-		int a=sc.nextInt();
-		int b=sc.nextInt();
-		int count=0;
-		for(int i=0;i<9;i++){
-		count++;
-		}
-		int a=a+b;
-		int b=b+a;
+        int n = 10; 
+        int firstTerm = 0;
+        int secondTerm = 1;
+
+        System.out.println("Fibonacci Series up to " + n + " terms:");
+
+        for (int i = 1; i <= n; ++i) {
+            System.out.print(firstTerm + ", ");
+            int nextTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+        }
 		}
 
 
