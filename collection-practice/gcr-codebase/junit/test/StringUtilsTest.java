628cac55f3590d3c9be59ad26015ffd5b7a0e1f6
himanshu <anshmishra75436@gmail.com>
2026-01-31 21:25:34 +0530

    [Himanshu Mishra] Add. junit problems

---
+++ collection-practice/gcr-codebase/junit/test/StringUtilsTest.java
@@ -0,0 +1,23 @@
+package BridgeLabz_Training.Junit;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+public class StringUtilsTest {
+StringUtils utils=new StringUtils();
+@Test
+void testReverse() {
+	assertEquals("olleh",utils.reverse("hello"));
+}
+@Test
+void testPalindromeTrue() {
+	assertTrue(utils.isPalindrome("madam"));
+}
+@Test
+void testPalindromeFalse() {
+	assertFalse(utils.isPalindrome("hello"));
+}
+@Test
+void testUpperCase() {
+	assertEquals("JAVA",utils.toUpperCase("java"));
+
+}
+}
